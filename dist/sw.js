410ab7b29787c98bcf0f9342dc52112a25edb0bf
thehambugermanreal <13develoepr@tutamail.com>
2026-09-14 21:10:15 -0500

    v0

--- dist/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('/controller/controller.sw.js')
-
-self.addEventListener('fetch', (event) => {
-  if (typeof $scramjetController !== 'undefined' && $scramjetController.shouldRoute(event)) {
-    event.respondWith($scramjetController.route(event))
-  }
-})
